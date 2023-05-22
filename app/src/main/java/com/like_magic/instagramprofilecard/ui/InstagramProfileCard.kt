f8c04942808a8c53f372e273326ed430be196683
LikeMagic85 <like-magic@yandex.ru>
2023-05-22 17:17:51 +0300

    Compose card is implemented

--- app/src/main/java/com/like_magic/instagramprofilecard/ui/InstagramProfileCard.kt
+++ app/src/main/java/com/like_magic/instagramprofilecard/ui/InstagramProfileCard.kt
@@ -1,45 +1,66 @@
 package com.like_magic.instagramprofilecard.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(modifier = Modifier.fillMaxHeight()) {
+    Card(
+        shape = RoundedCornerShape(
+            topStart = 4.dp,
+            topEnd = 4.dp
+        ),
+        border = BorderStroke(1.dp, Color.Black),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White
+        ),
+        modifier = Modifier.padding(4.dp)
+    ) {
+        Row(modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
 
+        ) {
+            Box(
+                modifier = Modifier.size(50.dp)
+            )
+            SetValue(topText = "6950", bottomText = "Posts")
+            SetValue(topText = "436M", bottomText = "Followers")
+            SetValue(topText = "78", bottomText = "Following")
         }
-        Column() {
-            Box() {
-                Text(text = "6950")
-            }
-            Box() {
-                Text(text = "Posts")
-            }
+    }
+
+}
+
+@Composable
+private fun SetValue(topText:String, bottomText:String){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box() {
+            Text(text = topText)
         }
-        Column() {
-            Box() {
-                Text(text = "436M")
-            }
-            Box() {
-                Text(text = "followers")
-            }
-        }
-        Column() {
-            Box() {
-                Text(text = "76")
-            }
-            Box() {
-                Text(text = "following")
-            }
+        Box() {
+            Text(text = bottomText)
         }
     }
 }
